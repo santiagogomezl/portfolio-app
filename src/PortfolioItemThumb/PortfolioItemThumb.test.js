1d59d40d112f14aee1597abbc7fc163ec3d99597
Santiago Gomez <santiagogomezl@Santiagos-MBP.attlocal.net>
2020-12-06 21:05:45 -0800

    Portfolio v1.1

--- src/PortfolioItemThumb/PortfolioItemThumb.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import PortfolioItem from './PortfolioItem'
-
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<PortfolioItem />, div)
-  ReactDOM.unmountComponentAtNode(div)
-})
